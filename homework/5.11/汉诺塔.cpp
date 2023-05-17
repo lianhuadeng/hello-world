472ae1f66a5cd8168cfd4888428d2d2dbb068832
lianhuadeng <2171822821@qq.com>
2023-05-18 00:09:35 +0800

    homework/ commit

--- homework/5.11/汉诺塔.cpp
+++ homework/5.11/汉诺塔.cpp
@@ -6,17 +6,28 @@
 2. 大盘不能叠在小盘上面；
 了.可将圆盛临时置于B杆，也可将从A杆移出的圆盘重新移回A杆，但都必须遵待上述两条规则。
 */
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-class tower
-{
-public:
+int temp=0;
+int move(int n,char a,char b,char c){
 	
-};
+	if(n==0){
+		
+		return 0;//函数出口 
+	}
+	move(n-1,a,c,b);//第一步，将n-1个盘子从A柱移动至B柱（C柱为工具柱） 
+	temp++;//输出移动过程前计数 
+	cout<<a<<"--->"<<c<<endl;//输出移动过程 
+	move(n-1,b,a,c);//第二步将B柱的n-1个盘子移动至C柱（A柱为工具柱） 
+//	cout<<b<<"--->"<<c<<endl; 
+}
+
 
 int main(){
-	
-
+	int n;
+	char a='A',b='B',c='C';
+	cin>>n;
+	move(n,a,b,c);	
+	cout<<temp;
 	return 0;
 }
